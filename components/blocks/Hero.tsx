10fa99f975b8f40995ab5512190235088a76657e
Salim <sidymamadousalim@gmail.com>
2024-06-05 16:56:47 +0100

    resolve hero

--- components/blocks/Hero.tsx
+++ components/blocks/Hero.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import React from "react";
 import Link from "next/link";
 import SearchHero from "./SearchHero";
-import { ImagesSlider } from "@/components/ui/acertiny/image-slider";
+import { ImagesSlider } from "../ui/acertiny/image-slider";
 
 type Props = {};
 
