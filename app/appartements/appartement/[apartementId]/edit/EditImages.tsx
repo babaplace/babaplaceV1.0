af84fd444286af811f04b027a575df9fa5dc5b0e
Salim <sidymamadousalim@gmail.com>
2024-06-07 00:01:15 +0100

    start add images edit

--- app/appartements/appartement/[apartementId]/edit/EditImages.tsx
+++ app/appartements/appartement/[apartementId]/edit/EditImages.tsx
@@ -1,7 +1,9 @@
+"use client";
 import { medias } from "@prisma/client";
 import Image from "next/image";
 import React from "react";
 import Link from "next/link";
+import { useEditAppartementState } from "../../../../../lib/zustand/stores/editAppartementStore";
 
 type Props = {
   images: medias[];
@@ -9,16 +11,22 @@ type Props = {
 };
 
 const EditImages = ({ images, appartementId }: Props) => {
+  const { startEditImages, setStartEditImages } = useEditAppartementState();
   return (
     <div className="border border-gray-100 rounded-lg p-4">
       <div className="flex justify-between items-start">
         <h1 className="text-xl font-extrabold">Images</h1>
-        <Link
-          href={"/appartements/appartement/add/imagesStep"}
-          className="w-fit text-gray-500hover:text-primary text-sm underline justify-end"
-        >
-          Modifier
-        </Link>
+        {!startEditImages ? (
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              //   setStartEditDetailsInfos(true);
+            }}
+            className="w-fit text-gray-500hover:text-primary text-sm underline justify-end"
+          >
+            Modifier
+          </button>
+        ) : null}
       </div>
       <div className="divide-y-2 divide-dotted flex flex-col ">
         {images.length > 0 ? ( // Conditional rendering based on uploadedImages state
