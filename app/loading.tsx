d04374117f1f32c3111e53bc3b0286648aebeaf4
Salim <sidymamadousalim@gmail.com>
2024-06-07 18:57:24 +0100

    remove loadinng chunck

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,6 @@
 import Loader from "@/components/ui/Loader";
 
 export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
   return (
     <div className="h-screen w-full flex justify-center items-center">
       <Loader />
--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,14 @@ import CardPropertyA from "@/components/blocks/CardPropertyA";
 import Hero from "@/components/blocks/Hero";
 import SectionPropertiesA from "@/components/blocks/SectionPropertiesA";
 import Services from "@/components/blocks/Services";
-import { Suspense } from "react";
-import SectionPropertiesASkeleton from "@/components/skeletons/PropertiesSectionASkeleton";
 
 export default async function Home() {
   return (
     <main>
       <Hero />
-      <Suspense fallback={<SectionPropertiesASkeleton />}>
-        <SectionPropertiesA />
-      </Suspense>
+      {/* <Suspense fallback={<SectionPropertiesASkeleton />}> */}
+      <SectionPropertiesA />
+      {/* </Suspense> */}
       <Services />
     </main>
   );
