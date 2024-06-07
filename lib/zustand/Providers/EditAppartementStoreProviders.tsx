030ff07e48b0d0098edd40aa8f9553e4d83101bf
Salim <sidymamadousalim@gmail.com>
2024-06-07 20:33:35 +0100

    add provider for store3

--- lib/zustand/Providers/EditAppartementStoreProviders.tsx
+++ lib/zustand/Providers/EditAppartementStoreProviders.tsx
@@ -3,7 +3,7 @@
 import { type ReactNode, createContext, useRef, useContext } from "react";
 import { type StoreApi, useStore } from "zustand";
 import {
-  type EditAppartementStore,
+  EditAppartementStore,
   createEditAppartementStore,
 } from "../stores/editAppartementStore";
 
